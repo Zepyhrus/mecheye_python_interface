cfba1c78a329515415328c51ef3a9413a095bae0
mechmind-yx <yang.xin@mech-mind.net>
2021-01-06 13:00:39 +0800

    增加深度信息使用注释

--- sample.py
+++ sample.py
@@ -5,7 +5,6 @@ import cv2
 import numpy as np
 import os
 import open3d
-import yaml
 
 SAVE_PATH = "D:"
 
@@ -14,7 +13,7 @@ if __name__ == '__main__':
     camera = CameraClient()
     save_file = True
     # camera ip should be modified to actual ip address
-    camera_ip = "192.168.3.189"
+    camera_ip = "192.168.3.226"
     # always set ip before do anything else
     if not camera.connect(camera_ip):
         exit(-1)
@@ -25,8 +24,10 @@ if __name__ == '__main__':
     print ("Version: %s" % (camera.getCameraVersion()))
 
     # capture depth image and color image and save them
-    color = camera.captureColorImg()
     depth = camera.captureDepthImg()
+    color = camera.captureColorImg()
+<<<<<<< HEAD
+    depth = camera.captureDepthImg()  #depth data
     # save depth data
     with open('output.yaml', 'w') as f:
         yaml.dump({"depth_data": np.array(depth)}, f)
@@ -35,12 +36,17 @@ if __name__ == '__main__':
         depth_data = np.array(yaml.load(f,Loader=yaml.Loader)['depth_data'])
         print(depth_data)
     
+=======
+
+>>>>>>> parent of 5b4f4b6 (增加深度信息使用注释)
     if len(depth) == 0 or len(color) == 0:
         exit(-2)
 
     if save_file:
         if not os.path.exists(SAVE_PATH):
             os.makedirs(SAVE_PATH)
+        # The data format of the depth map saved by opencv is uint8, the black area represents no depth information, and the white area represents depth information; 
+        # if you need to use depth data, directly use the depth in line 29 of the program "depth = camera.captureDepthImg()"
         cv2.imwrite(SAVE_PATH + "/mechmind_depth.png", depth)
         cv2.imwrite(SAVE_PATH + "/mechmind_color.jpg", color)
     # set some parameters of camera, you can refer to parameters' names in Mech_eye
@@ -54,4 +60,7 @@ if __name__ == '__main__':
     print(camera.setParameter("scan2dExposureTime",20)) # set exposure time to 20ms
     print(camera.getParameter("scan2dExposureTime"))
     exit(0)
+<<<<<<< HEAD
     
+=======
+>>>>>>> parent of 5b4f4b6 (增加深度信息使用注释)
