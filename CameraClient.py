2292fe302b936a516e12ba1a29a42c49c9dbdb60
ubuntu <sherkfung@gmail.com>
2021-08-25 14:39:32 +0800

    support v1.1.1

--- CameraClient.py
+++ CameraClient.py
@@ -237,6 +237,11 @@ class CameraClient(ZmqClient):
         depth.flatten()
         depth = depth * 0.001
         depth.resize(int(np.size(depth)/3),3)
+
+        _i = depth[:, 2] > 0.01
+        depth = depth[_i, :]
+        color = color[_i, :]
+
         test_pcd.points = open3d.utility.Vector3dVector(depth)  # 定义点云坐标位置
         test_pcd.colors = open3d.utility.Vector3dVector(color)  # 定义点云的颜色
         return test_pcd
--- sample.py
+++ sample.py
@@ -6,43 +6,65 @@ import numpy as np
 import os
 import open3d
 
-SAVE_PATH = "D:"
+import matplotlib.pyplot as plt
+plt.rcParams['figure.autolayout'] = True
+
+SAVE_PATH = "data"
 
 if __name__ == '__main__':
 
     camera = CameraClient()
     save_file = True
     # camera ip should be modified to actual ip address
-    camera_ip = "192.168.3.226"
+    camera_ip = "192.168.1.13"
     # always set ip before do anything else
     if not camera.connect(camera_ip):
         exit(-1)
-    # get some camera info like intrincis, ip, id and Version
-    intri = camera.getCameraIntri()
-    print ("Camera Info: %s" % (camera.getCameraInfo()))
-    print ("Camera ID: %s" % (camera.getCameraId()))
-    print ("Version: %s" % (camera.getCameraVersion()))
+    # # get some camera info like intrincis, ip, id and Version
+    # intri = camera.getCameraIntri()
+    # print ("Camera Info: %s" % (camera.getCameraInfo()))
+    # print ("Camera ID: %s" % (camera.getCameraId()))
+    # print ("Version: %s" % (camera.getCameraVersion()))
 
-    # capture depth image and color image and save them
-    depth = camera.captureDepthImg()
-    color = camera.captureColorImg()
+    # # capture depth image and color image and save them
+    # depth = camera.captureDepthImg()
+    # color = camera.captureColorImg()
 
-    if len(depth) == 0 or len(color) == 0:
-        exit(-2)
+    # if len(depth) == 0 or len(color) == 0:
+    #     exit(-2)
 
-    if save_file:
-        if not os.path.exists(SAVE_PATH):
-            os.makedirs(SAVE_PATH)
-        cv2.imwrite(SAVE_PATH + "/mechmind_depth.png", depth)
-        cv2.imwrite(SAVE_PATH + "/mechmind_color.jpg", color)
-    # set some parameters of camera, you can refer to parameters' names in Mech_eye
+    # if save_file:
+    #     if not os.path.exists(SAVE_PATH):
+    #         os.makedirs(SAVE_PATH)
+    #     cv2.imwrite(SAVE_PATH + "/mechmind_depth.png", depth)
+    #     cv2.imwrite(SAVE_PATH + "/mechmind_color.jpg", color)
+    # # set some parameters of camera, you can refer to parameters' names in Mech_eye
 
+
+    print(camera.setParameter("scan2dExposureTime", 100)) # set exposure time to 20ms
+    print(camera.getParameter("scan2dExposureTime"))
     # get rgb point cloud. Using open3d to store and visualize the cloud.
     pcd = camera.captureCloud()
+
+    # p = np.asarray(pcd.points)
+    # plt.hist(p[:, 2], np.arange(100)/100)
+    # plt.show()
+
     open3d.visualization.draw_geometries([pcd])
-    # set some parameters of camera, you can refer to parameters' names in Mech_eye
-    print(camera.setParameter("scan2dExposureMode",0)) # set exposure mode to Timed
-    print(camera.getParameter("scan2dExposureMode"))
-    print(camera.setParameter("scan2dExposureTime",20)) # set exposure time to 20ms
-    print(camera.getParameter("scan2dExposureTime"))
+
+
+    # # set some parameters of camera, you can refer to parameters' names in Mech_eye
+    # print(camera.setParameter("scan2dExposureMode",0)) # set exposure mode to Timed
+    # print(camera.getParameter("scan2dExposureMode"))
+
+    # # get rgb point cloud. Using open3d to store and visualize the cloud.
+    # pcd = camera.captureCloud()
+    # open3d.visualization.draw_geometries([pcd])
+
+    # print(camera.setParameter("scan2dExposureTime",20)) # set exposure time to 20ms
+    # print(camera.getParameter("scan2dExposureTime"))
+
+    # # get rgb point cloud. Using open3d to store and visualize the cloud.
+    # pcd = camera.captureCloud()
+    # open3d.visualization.draw_geometries([pcd])
     exit(0)
